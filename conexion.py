1f499d6e7edfeb9f9b43efc25112bf0540f25fb9
habds <matiasmallealira@gmail.com>
2020-05-21 03:49:40 -0400

    conectado a bd

--- conexion.py
+++ conexion.py
@@ -1,16 +1,21 @@
 import psycopg2
 #conexion = psycopg2.connect("dbname=dah4ovgnh5u7uj user=scpuhirqcifnzq password=ac1f425c77c31e67ae108b18f58baeb86b6a25e185b48382ef6f8876b1f5bcca port=5432")
-conexion = psycopg2.connect(host="ec2-50-17-21-170.compute-1.amazonaws.com", database="dah4ovgnh5u7uj", user="scpuhirqcifnzq", password="ac1f425c77c31e67ae108b18f58baeb86b6a25e185b48382ef6f8876b1f5bcca")
 
-# Creamos el cursor con el objeto conexion
-cur = conexion.cursor()
 
 # Ejecutamos una consulta
-cur.execute( "select id,codigo,descripcion from metodo_pago" )
+
 
 # Recorremos los resultados y los mostramos
-for nombre in cur.fetchall() :
-    print(nombre)
+#for nombre in cur.fetchall() :
+#    print(nombre)
 
+
+def select(query):
+    conexion = psycopg2.connect(host="ec2-50-17-21-170.compute-1.amazonaws.com", database="dah4ovgnh5u7uj", user="scpuhirqcifnzq", password="ac1f425c77c31e67ae108b18f58baeb86b6a25e185b48382ef6f8876b1f5bcca")
+    # Creamos el cursor con el objeto conexion  
+    cur = conexion.cursor()
+    respuesta=cur.execute( "query" )
+    conexion.close()
+    return respuesta
+    
 # Cerramos la conexión
-conexion.close()
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, jsonify
+from conexion import *
 # from bdfalsa import productos
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return 'hjola' #jsonify(productos)
+    return 'hola' #jsonify(productos)
+
+@app.route('/metodo_pago')
+def metodoPago():
+    respuesta=select("select descripcion from forma_pago")
+    return jsonify(respuesta)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
