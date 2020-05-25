55f5d1a2d40ba14300f44fcbde86b072a632ca43
QuesoCaliente <brianc.contacto@gmail.com>
2020-05-24 20:45:31 -0400

    conexion modificado, ciudad modificado

--- Clases/Ciudad.py
+++ Clases/Ciudad.py
@@ -1,7 +1,13 @@
+import sys, os
+sys.path.append(os.getcwd())
+from conexion import DataBaseConexion
 class Ciudad(): 
-   def __init__(self,id = 0,nombre = ''): 
+
+   def __init__(self,id = 0,nombre = '', idRegion=0): 
       self.id = id
       self.nombre = nombre
+      self.idRegion = idRegion
+      self.db = DataBaseConexion()
  
    def setId(self, id):
       self.id = id
@@ -14,6 +20,17 @@ class Ciudad():
  
    def getNombre(self):
       return self.nombre
- 
+
+   def getCiudad(self):
+      self.db.cursor.execute(f'select id, nombre, idRegion from ciudad where nombre="{self.nombre}"')
+      obj = self.db.cursor.fetchone()
+      setId(f'{obj[0]}')
+      setNombre(f'{obj[1]}')
+      setId(f'{obj[2]}')
+
+
    def __str__(self):
       return str(self.id), str(self.nombre)
+
+
+
--- conexion.py
+++ conexion.py
@@ -1,29 +1,14 @@
-import psycopg2
-#conexion = psycopg2.connect("dbname=dah4ovgnh5u7uj user=scpuhirqcifnzq password=ac1f425c77c31e67ae108b18f58baeb86b6a25e185b48382ef6f8876b1f5bcca port=5432")
+import mysql.connector
 
-
-# Ejecutamos una consulta
-
-
-# Recorremos los resultados y los mostramos
-#for nombre in cur.fetchall() :
-#    print(nombre)
-
-
-def select():
-    conexion = psycopg2.connect(host="ec2-50-17-21-170.compute-1.amazonaws.com", database="dah4ovgnh5u7uj", user="scpuhirqcifnzq", password="ac1f425c77c31e67ae108b18f58baeb86b6a25e185b48382ef6f8876b1f5bcca")
-    # Creamos el cursor con el objeto conexion  
-    respuesta=[]
-    cur = conexion.cursor()
-    cur.execute("select descripcion from metodo_pago")
-    for nombre in cur.fetchall() :
-        respuesta.append(nombre)
-        conexion.close()
-    return respuesta
-
-def coneccion():
-    conexion = psycopg2.connect(host="ec2-50-17-21-170.compute-1.amazonaws.com", database="dah4ovgnh5u7uj", user="scpuhirqcifnzq", password="ac1f425c77c31e67ae108b18f58baeb86b6a25e185b48382ef6f8876b1f5bcca")
-    
-    return conexion
-    
-# Cerramos la conexión
+class DataBaseConexion():
+    def __init__(self):
+        try:
+            self.db = mysql.connector.connect(host='localhost', port=3306, user='root', password='12345', database='baratappdb')
+            self.cursor = self.db.cursor()
+            print('Conexion Exitosa')
+        except mysql.connector.Error as err:
+            print("Ha ocurrido un error: {}".format(err))
+        
+    def getData(self, query):
+        self.db.cursor.execute(query)
+        return self.db.cursor.fetchall()
