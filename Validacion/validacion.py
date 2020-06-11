ec6af361372401bd377fdb0877aa1172d82c8114
habds <matiasmallealira@gmail.com>
2020-06-11 15:36:39 -0400

    <sdsd

--- Validacion/validacion.py
+++ Validacion/validacion.py
@@ -2,7 +2,10 @@ from flask import Flask, request, make_response
 import jwt
 
 def login():
+    #asdgashjdgahjsgdajds
     auth= request.authorization
 
     if auth.username=='usuario' and auth.password=='password':
         token = jwt.encode({'user' : auth.username}
+
+        
